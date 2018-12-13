140b8e1103d69c6221f6d95b744849d143205c2b
Inbar Shiloh <inbarshiloh@campus.technion.ac.il>
2018-12-13 19:31:25 +0200

    Ben's test

---
+++ src/test/java/technify/MostPopularSongTest.java
@@ -0,0 +1,51 @@
+package technify;
+
+import org.junit.Test;
+import technify.business.Playlist;
+import technify.business.Song;
+import technify.business.User;
+
+import static org.junit.Assert.assertEquals;
+import static technify.business.ReturnValue.*;
+import static technify.printer.selectTable;
+
+
+public class MostPopularSongTest extends  AbstractTest {
+    @Test
+    public void basicPopular()
+    {
+        Song s = new Song();
+        s.setId(1);
+        s.setName("Winning song");
+        s.setGenre("A");
+        s.setCountry("Aa");
+        s.setPlayCount(0);
+        assertEquals(OK, Solution.addSong(s));
+
+        Song s2 = new Song();
+        s2.setId(2);
+        s2.setName("Losing song");
+        s2.setGenre("A");
+        s2.setCountry("Aa");
+        s2.setPlayCount(0);
+        assertEquals(OK, Solution.addSong(s2));
+
+        Playlist p = new Playlist();
+        p.setId(1);
+        p.setGenre("A");
+        p.setDescription("aaa aaa");
+        assertEquals(OK, Solution.addPlaylist(p));
+
+        Playlist p2 = new Playlist();
+        p2.setId(2);
+        p2.setGenre("A");
+        p2.setDescription("aaa aaa");
+        assertEquals(OK, Solution.addPlaylist(p2));
+
+        assertEquals(OK, Solution.addSongToPlaylist(1,1));
+        assertEquals(OK, Solution.addSongToPlaylist(1,2));
+        assertEquals(OK, Solution.addSongToPlaylist(2,1));
+
+        assertEquals("Winning song", Solution.getMostPopularSong());
+    }
+}
